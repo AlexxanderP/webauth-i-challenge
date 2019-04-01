9d494b07b18374f91bf476e99e062b63278b78aa
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-04-01 15:53:24 -0700

    tested server with GET request, created POST request that uses bcrypt.js library to hash password this is passed in

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const server = require("./api/server.js");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-// server.use(helmet());
-// server.use(express.json());
-// server.use(cors());
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("My server is working!");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  /// hash the password
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 const port = process.env.PORT || 6500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
