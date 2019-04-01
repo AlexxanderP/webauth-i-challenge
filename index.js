f879f1a10dd9d5f8fa2e97e9b76cf6c4264ef612
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-04-01 16:10:05 -0700

    Middleware added that checks for credentials and one to only allow specific user to access, also restricted endpoint api/users

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+//----- Get Request to Test Server-----//
 server.get("/", (req, res) => {
   res.send("My server is working!");
 });
 
+//------ Post request for Register, included Bcrypt for Hashing------//
 server.post("/api/register", (req, res) => {
   let user = req.body;
 
@@ -29,6 +31,7 @@ server.post("/api/register", (req, res) => {
     });
 });
 
+//----- Post Request for Login------//
 server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
@@ -41,6 +44,54 @@ server.post("/api/login", (req, res) => {
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//----- Middleware That Checks on Credentials Before Allowing Authentication-----//
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check the password against the database
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Ran into an unexpected error" });
+      });
+    next();
+  } else {
+    res.status(401).json({ message: "No credentials provided" });
+  }
+}
+
+//------ Middleware Only Allowing Specific Users to Have Access------//
+function only(username) {
+  return function(req, res, next) {
+    if (req.headers.username === username) {
+      next();
+    } else {
+      res.status(403).json({
+        message: `You don't have access because you are not ${username}!`
+      });
+    }
+  };
+}
+
+//------restricts access to the '/api/login' endpoint to users that are authenticated-----//
+server.get("/api/users", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
     })
     .catch(error => {
       res.status(500).json(error);
