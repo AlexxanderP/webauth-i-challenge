27b619066c8f35544936ce8554755d93dafefaba
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-04-02 16:16:26 -0700

    session config added, get request for logout created, some refactoring done as well

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+const configuredKnex = require("./database/dbConfig.js");
+
 const server = express();
 
 const users = require("./database/dbConfig.js");
@@ -16,11 +20,32 @@ server.get("/", (req, res) => {
   res.send("My server is working!");
 });
 
+//------- Session Config------//
+const sessionConfig = {
+  name: "configuration",
+  secret: "Everyone has secrets. So whats yours? Girl on fire...",
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: configuredKnex,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30
+  })
+};
+
 //------ Post request for Register, included Bcrypt for Hashing------//
 server.post("/api/register", (req, res) => {
-  let { username, password } = req.body;
+  let user = req.body;
 
-  password = bcrypt.hashSync(password, 10);
+  const hash = (user.password = bcrypt.hashSync(user.password, 10));
+  user.password = hash;
 
   users("users")
     .insert(req.body)
@@ -57,6 +82,21 @@ server.post("/api/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+//------ Get Request for Logout------//
+server.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "there was a problem." });
+      } else {
+        res.status(200).json({ message: "bye, thanks for coming!" });
+      }
+    });
+  } else {
+    res.status(200).json({ message: "bye, thanks for coming!" });
+  }
 });
 
 //----- Middleware That Checks on Credentials Before Allowing Authentication-----//
@@ -106,6 +146,6 @@ server.get("/api/user", restricted, (req, res) => {
       res.status(500).json(error);
     });
 });
-
+server.use(session(sessionConfig));
 const port = process.env.PORT || 6500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
